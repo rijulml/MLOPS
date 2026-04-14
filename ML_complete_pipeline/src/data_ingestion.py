2e10ee6973704f9cb2d069a1a7c43c0f4a873749
rijulml <rijulbajaj.ml@gmail.com>
2026-04-14 16:12:17 +0530

    Data Ingestion Module Done for End to End ML Pipeline

---
+++ ML_complete_pipeline/src/data_ingestion.py
@@ -0,0 +1,71 @@
+import numpy as np        # For numerical operations
+import pandas as pd       # For data manipulation and analysis
+import os
+from sklearn.model_selection import train_test_split
+import logging
+
+
+# Logging 
+log_dir = '../logs'
+os.makedirs(log_dir, exist_ok=True)
+
+logger = logging.getLogger('data_ingestion')
+
+logger.setLevel('DEBUG')
+
+filepath = os.path.join(log_dir,'data_ingestion.log')
+file_handler = logging.FileHandler(filepath)
+file_handler.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_data(data_url:str)->pd.DataFrame:
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug("Data Loaded from %s", data_url)
+        return df
+    except Exception as e:
+        logger.error("Unexpected error occured while loading the data: %s", e)
+        return
+    
+def data_processing(dataframe:pd.DataFrame)->pd.DataFrame:
+    try:
+        dataframe.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'], inplace=True)
+        dataframe.rename(columns = {'v1':'target', 'v2':'text'}, inplace=True)
+        logger.debug('Data Processing completed')
+        return dataframe
+    except Exception as e:
+        logger.error(f'Unexpected Error occured while data processing {e}')
+        raise ValueError (f'Unexpected Error occured while data processing {e}')
+    
+def save_data(train_data:pd.DataFrame, test_data:pd.DataFrame, data_path:str)->None:
+    try:
+        raw_data_path = os.path.join(data_path, 'raw')
+        os.makedirs(raw_data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path, 'train_data.csv'), index=False)
+        test_data.to_csv(os.path.join(raw_data_path, 'test_data.csv'), index=False)
+        logger.debug('Train and Test data saved to %s', raw_data_path)
+    except Exception as e:
+        logger.error('Unexpected error while saving the data %s', e)
+
+def main():
+    try:
+        test_size = 0.2
+        data_url='https://raw.githubusercontent.com/vikashishere/YT-MLOPS-Complete-ML-Pipeline/refs/heads/main/experiments/spam.csv'
+        df = load_data(data_url=data_url)
+        processed_data = data_processing(df)
+        train_data, test_data = train_test_split(processed_data, test_size=test_size, random_state=42)
+        save_data(train_data, test_data, data_path='../data')
+    except Exception as e:
+        logger.error('Failed to complete the data ingestion process %s', e)
+
+if __name__ == '__main__':
+    main()
